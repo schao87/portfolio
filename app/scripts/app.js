cc79930a7ca8601ea41f10eaf9935a1185a20f5c
schao87 <schaosti@gmail.com>
2014-12-02 18:44:36 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfolioApp
+ * @description
+ * # portfolioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portfolioApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
